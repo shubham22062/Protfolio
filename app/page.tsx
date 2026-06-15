89130013e6c244974a48f9a37a290bf9a9e84143
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-15 16:02:11 +0530

    Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Landing from "@/components/Landing";
 //import Navbar from "@/components/Navbar";
 
+
 export default function Home() {
   return (
        <div>
          <Landing/>
+        
          
        </div>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+  export default function Navbar(){
+    return (
+        <nav>
+            <h1>This is navbar!!!</h1>
+        </nav>
+    )
+  }
