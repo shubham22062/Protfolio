20613232af8e11661250f830a5604126c87011a1
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-18 15:26:22 +0530

    changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,45 @@
 "use client";
 
-import { useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import { motion, useInView } from "framer-motion";
 import { Typewriter } from "react-simple-typewriter";
+import {
+  SpeakerWaveIcon,
+  SpeakerXMarkIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Hero() {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const heroRef = useRef<HTMLDivElement>(null);
+
   const [isPlaying, setIsPlaying] = useState(true);
   const [hasEnded, setHasEnded] = useState(false);
+
+  const [isMuted, setIsMuted] = useState(true);
+
+  const toggleMute = () =>{
+    if(!videoRef.current) return;
+
+    videoRef.current.muted = !videoRef.current.muted;
+    setIsMuted(videoRef.current.muted)
+  }
+
+
+  const isInView = useInView(heroRef, {
+    amount: 0.4,
+  });
+
+  useEffect(() => {
+    if (!videoRef.current || hasEnded) return;
+
+    if (isInView) {
+      videoRef.current.play();
+      setIsPlaying(true);
+    } else {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    }
+  }, [isInView, hasEnded]);
 
   const toggleVideo = () => {
     if (!videoRef.current) return;
@@ -31,6 +63,7 @@ export default function Hero() {
 
   return (
     <motion.div
+      ref={heroRef}
       className="px-3 pt-5 flex relative min-h-screen"
       initial={{ opacity: 0, y: 120, scale: 0.96 }}
       whileInView={{ opacity: 1, y: 0, scale: 1 }}
@@ -44,7 +77,9 @@ export default function Hero() {
       <video
         ref={videoRef}
         autoPlay
+        muted
         playsInline
+        preload="auto"
         onEnded={() => {
           setHasEnded(true);
           setIsPlaying(false);
@@ -53,7 +88,7 @@ export default function Hero() {
         onPause={() => {
           if (!hasEnded) setIsPlaying(false);
         }}
-        className="h-[800px]"
+        className="h-[780px] mt-[20px] ml-[20px] object-cover video-mask opacity-90"
       >
         <source
           src="https://res.cloudinary.com/dbdvicxun/video/upload/v1781599530/b_Animate_the_characte_online-video-cutter.com_fb230s.mp4"
@@ -63,6 +98,14 @@ export default function Hero() {
 
       {/* GRADIENT */}
       <div className="absolute bottom-0 left-0 w-full h-40 bg-gradient-to-t from-black to-transparent" />
+
+        <button onClick={toggleMute}>
+            {isMuted ? (
+                <SpeakerXMarkIcon className="h-7 w-7 text-white hover:scale-105 transition-transform duration-300" />
+            ) : (
+                <SpeakerWaveIcon className="h-7 w-7 text-white hover:scale-105 transition-transform duration-300" />
+            )}
+        </button>
 
       {/* TEXT SECTION */}
       <div className="pt-70 pl-30">
@@ -81,27 +124,28 @@ export default function Hero() {
         </motion.span>
 
         {/* TITLE */}
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ amount: 0.5 }}
-        transition={{ duration: 0.8 }}
-        className="text-5xl font-bold text-white text-center pt-4"
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ amount: 0.5 }}
+          transition={{ duration: 0.8 }}
+          className="text-5xl font-bold text-white text-center pt-4"
         >
-        <Typewriter
+          <Typewriter
             words={[
-            "Software Engineer",
-            "Full Stack Developer",
-            "AI Enthusiast",
-            "Problem Solver",
+              "Software Engineer",
+              "Full Stack Developer",
+              "Bulid Scalable web App",
+              "AI Enthusiast",
+              "Problem Solver",
             ]}
-            loop={0} // infinite
+            loop={0}
             cursor
             cursorStyle="|"
             typeSpeed={80}
             deleteSpeed={50}
             delaySpeed={1200}
-        />
+          />
         </motion.div>
 
         {/* DESCRIPTION */}
@@ -114,7 +158,7 @@ export default function Hero() {
             delay: 0.4,
             ease: "easeOut",
           }}
-          className="text-lg text-gray-400 max-w-3xl pt-8 text-center"
+          className="text-lg text-gray-400 max-w-2xl pt-8 text-center"
         >
           Driven by curiosity and innovation, I build modern web applications
           that combine clean design with powerful functionality. With expertise
@@ -132,7 +176,7 @@ export default function Hero() {
             delay: 0.7,
             ease: "easeOut",
           }}
-          className="flex items-center gap-6 mt-[80px] pl-10"
+          className="flex items-center gap-6 mt-[50px] pl-10"
         >
           <button
             onClick={toggleVideo}
@@ -158,10 +202,11 @@ export default function Hero() {
                     d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
                   />
                 </svg>
+
                 <span>Resume</span>
               </div>
 
-              <span className="absolute -bottom-8 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all duration-200 text-xs px-2 py-1 rounded-md whitespace-nowrap bg-black text-white dark:bg-white dark:text-black">
+              <span className="absolute -bottom-8 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition-all duration-200 text-xs px-2 py-1 rounded-md whitespace-nowrap bg-black text-white">
                 Resume
               </span>
             </a>
