98ac2ccd9e9b6b0b694a2b446636f371be44f78e
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-22 23:41:46 +0530

    some changes

--- components/About.tsx
+++ components/About.tsx
@@ -5,6 +5,7 @@ export default function About(){
             <div className="border-2 bg-amber-50 border-amber-50 w-[1200px] h-[700px] ml-[100px] rounded-md">
                 <h1 className="grid text-6xl font-semibold text-red-500">About</h1>
                 <p className="text-red-400">This is my page..</p>
+                <p className="text-4xl text-red-500">I'm gone a show you what is real portfolio...</p>
 
             </div>
         </div>
