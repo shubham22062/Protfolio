1c2876e3e502f5a50697b7f0cf4bf7bc1ef26ba5
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-06-30 23:11:38 +0530

    changes

--- components/About.tsx
+++ components/About.tsx
@@ -2,14 +2,7 @@ export default function About(){
     return(
         <div className="pt-40 ">
             
-            <div className="border-2 bg-amber-50 border-amber-50 w-[1200px] h-[700px] ml-[100px] rounded-md">
-                <h1 className="grid text-6xl font-semibold text-red-500">About</h1>
-                <p className="text-red-400">This is my page..</p>
-               
-                <p>Hi I'm Shubham</p>
-                <p> A passinoate full stack developer..</p>
-
-            </div>
+            <h1>About</h1>
         </div>
     )
 }
