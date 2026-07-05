393af918267a5cdb479718bf204d959fd0cd00b6
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-05 23:19:01 +0530

    cards

---
+++ components/Card.tsx
@@ -0,0 +1,10 @@
+export default function Card(){
+    return (
+        <div className="mt-20">
+            <div className="border-2 border-amber-50 rounded-2xl w-[500px] h-[500px]">
+
+            </div>
+
+        </div>
+    )
+}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,7 +1,15 @@
+import Card from "./Card";
+
 export default function Projects(){
     return(
         <div className="pt-24">
-            <h1 className="text-center text-red-500 animate-bounce text-6xl">Featured Projects</h1>
+            <h1 className="text-center text-red-500 animate-bounce text-6xl font-bold">Stuff I Built</h1>
+
+            <div>
+                <Card/>
+            </div>
+
+
         </div>
     )
 }
