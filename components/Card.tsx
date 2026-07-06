a4d7b61a79199ea05761e3bc7dd8491e5bf8ae52
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-06 23:03:24 +0530

    changes

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ export default function Card(){
     return (
         <div className="mt-20">
             <div className="border-2 border-amber-50 rounded-2xl w-[500px] h-[500px] transition-transform duration-300 hover:scale-105">
-
+                <h1>lets gooo</h1>
             </div>
 
         </div>
