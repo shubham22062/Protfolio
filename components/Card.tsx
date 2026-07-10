cb53b4c7a04489c11bb68b88066956af3ffcdd64
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-10 22:23:08 +0530

    add image

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,7 @@ export default function Card(){
     return (
         <div className="mt-25">
             <div className="border-2 border-amber-50 rounded-2xl w-[400px] h-[400px] transition-transform duration-300 hover:scale-105">
-            
+             <img src="Meshspire.svg" alt="meshspire" className="w-[250px] h-[150px]"/>
             </div>
 
         </div>
