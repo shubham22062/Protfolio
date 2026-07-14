cf5f0b786733a2744d02a04547125b782293a0e4
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-14 16:58:34 +0530

    footer logo

---
+++ components/Fixedlogo.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { Canvas } from "@react-three/fiber"
+
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+function LogoModel(){
+    const {scene} = useGLTF("logo.glb");
+
+    return(
+
+    )
+}
+
+
+
+export default function Fixedlogo(){
+    return(
+
+    )
+}
