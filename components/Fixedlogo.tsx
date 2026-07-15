6d6696f7e061a901f7bac977d7ec77cecfbdf267
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-15 22:46:10 +0530

    fixes

--- components/Fixedlogo.tsx
+++ components/Fixedlogo.tsx
@@ -1,18 +1,32 @@
 "use client"
 
 import { Canvas } from "@react-three/fiber"
-
 import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
+import * as THREE from "three";
 
 function LogoModel(){
+
     const {scene} = useGLTF("logo.glb");
+
+    useEffect(() => {
+        scene.traverse((child) => {
+            if (child instanceof THREE.Mesh) {
+                child.material = new THREE.MeshStandardMaterial({
+                    color: "#ef4444", // Tailwind red-500
+                    metalness: 0.5,
+                    roughness: 0.3,
+                });
+            }
+        });
+    }, [scene]);
+
 
     return(
         <primitive
-        object={scene}
-        scale = {1.5}
-        position ={[0,0,0]}
-        
+            object={scene}
+            scale={1.5}
+            position={[0,0,0]}
         />
     )
 }
@@ -20,26 +34,26 @@ function LogoModel(){
 useGLTF.preload("logo.glb")
 
 
-
 export default function Fixedlogo(){
     return(
         <div className="pt-10 w-full h-[200px]">
-             <Canvas camera={{ position:[0,0,5] }}>
 
-        <ambientLight intensity={1.5}/>
-        <directionalLight 
-          position={[3,3,3]} 
-          intensity={2}
-        />
+            <Canvas camera={{ position:[0,0,5] }}>
 
-        <LogoModel/>
+                <ambientLight intensity={1.5}/>
 
-        <OrbitControls 
-          enableZoom={false}
-          
-        />
+                <directionalLight 
+                    position={[3,3,3]} 
+                    intensity={2}
+                />
 
-      </Canvas>
+                <LogoModel/>
+
+                <OrbitControls 
+                    enableZoom={false}
+                />
+
+            </Canvas>
 
         </div>
     )
