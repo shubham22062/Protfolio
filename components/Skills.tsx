ce3ca16304377a3f8012ea58a0eec99e7686722f
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-19 15:25:31 +0530

    changes'

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,6 +2,10 @@ export default function Skills(){
     return(
         <div className="pt-24">
             <h1 className="text-red-500 text-6xl animate-bounce font-bold text-center">Tech Arsenal</h1>
+
+            <div className="mt-30 ml-10">
+                <span className="px-8 py-4 border border-gray-400 text-white text-xl rounded-md">CORE_LANGUAGES</span>
+            </div>
         </div>
     )
 }
