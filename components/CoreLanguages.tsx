e1e3a1afad4fdefa95539514469d43f07c24d7e9
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-07-21 23:30:24 +0530

    core_language

--- components/CoreLanguages.tsx
+++ components/CoreLanguages.tsx
@@ -1,7 +1,16 @@
 export default function CoreLanguages(){
     return(
-        <h1>
-            Lnaguage
-        </h1>
+        <div className=" border border-gray-800 w-[400px] h-[100px] rounded-md p-2">
+              <div className="flex gap-4">
+                <div className=" border border-gray-800 w-[50px] h-[50px] rounded-md p-2">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" alt="typescript" className="w-8 h-8 rounded-md"/>
+              </div>
+
+              <div>
+                <span className="text-xl font-semibold text-red-500">Typescript</span>
+                <span className="text-gray-400 block text-lg">PROFICIENCY</span>
+              </div>
+              </div>
+        </div>
     )
 }
